23bc7d097a1a36b7fcea6fccbea9cecd54cfaffa
bc <2651424897@qq.com>
2025-03-28 17:06:53 +0800

    路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 // 导入 createRouter 方法创建 Router 实例
 // 导入 createWebHistory 方法创建 HTML5 History 模式的路由规则
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import 'vue-router'
 
@@ -9,7 +9,8 @@ import { staticRoutes } from '@/router/routes.js'
 // 创建 Router 实例
 const router = createRouter({
   // 创建 HTML5 History 模式的路由规则
-  history: createWebHistory(),
+  // history: createWebHistory(),
+  history: createWebHashHistory(),
   // 挂载使用的路由规则
   routes: staticRoutes,
   // 处理路由的滚动行为，进入一个新页面，都会始终滚动到页面顶部
@@ -20,4 +21,3 @@ const router = createRouter({
 
 // 导出路由
 export default router
-
